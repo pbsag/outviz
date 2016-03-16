51d227b18192c0391d65e29c713bb6218856b9a7
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-03-16 17:41:22 -0400

    Add initial validation plot
    This plot takes a link table with volume and count fields and plots a standard validation plot.
    A user can optionally color by a variable in the ouptut to show more fine-grained count analysis.

---
+++ R/plot_validation.R
@@ -0,0 +1,43 @@
+#' Link validation plot
+#'
+#'
+#' @param links Model link table as a \code{tidy} data frame, with each row
+#'   representing a single direction of a single link.
+#' @param volume Character string identifying the modeled volume in the link
+#'   table.
+#' @param count Character string identifying the calibration/validation counts
+#'   in the link table.
+#' @param color_field (Optional) character string identifying variable to color
+#'   observations by, for example facility type.
+#'
+#' @return A ggplot2 plot object
+#'
+#' @import ggplot2
+#'
+#' @examples
+#' plot_validation(links, "volume", "count")
+#' plot_validation(links, "volume", "count", color_field = "facility_type")
+#'
+#' @export
+plot_validation <- function(links, volume, count, color_field = NULL) {
+
+  # if split by color, then add factor variable of the color field
+  if(!is.null(color_field)){
+    l <- links %>%
+      mutate_(
+        "color" = lazyeval::interp(~ factor(var), var = as.name(color_field))
+      )
+
+    p <- ggplot(l, aes_string(x = count, y = volume, color = "color")) +
+      scale_color_brewer(color_field, type = "qual")
+  } else {
+    p <- ggplot(l, aes_string(x = volume, y = count))
+  }
+
+  # Add geometries for points and statistics, and return
+  p +
+    geom_point(alpha = 0.3) +
+    geom_abline(slope = 1, intercept = 0) +
+    stat_smooth()
+
+}
