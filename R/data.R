49e2993f38ce3e1789000ce930061c93fc6be878
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-03-16 19:54:04 -0400

    Add mdd curve data
    Addresses issue #2.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Maximum desirable deviation curve
+#'
+#' A dataset containing the maximum desirable error for links by volume.
+#'
+#' @format A data frame with 7 rows and 2 variables:
+#' \describe{
+#'   \item{volume}{the volume category of the road}
+#'   \item{mdd}{maximum desirable deviation, in percent}
+#' }
+#'
+"mdd"
