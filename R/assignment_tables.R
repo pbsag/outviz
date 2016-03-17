16c188a62aab03b8b7e60088efb30e722da739e0
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-03-17 16:13:58 -0400

    Add screenline results table
    Close #4

--- R/assignment_tables.R
+++ R/assignment_tables.R
@@ -63,5 +63,59 @@ table_rmse <- function(links, volume, count, group_field = NULL) {
 
 }
 
+#' Total flow table
+#'
+#' This table sums the volume on all links in a group, and compares against the
+#' total count volume, as in a screenline comparison.
+#'
+#' @inheritParams table_rmse
+#'
+#' @return A \code{data_frame} with the link summary table.
+#'
+#' @import dplyr
+#' @import lazyeval
+#'
+#' @examples
+#' table_rmse(links, "volume", "count")
+#'
+#' a <- table_flow(links, "volume", "count", group_field = "facility_group")
+#' knitr::kable(a)
+#'
+#' @export
+#'
+table_flow <- function(links, volume, count, group_field = NULL) {
 
+  # must supply group_field
+  if(is.null(group_field)){
+    stop("Must supply grouping variable")
+  }
 
+  # table by grouping
+  dots <- list(
+    lazyeval::interp(~sum(x), x = as.name(volume)),
+    lazyeval::interp(~sum(x), x = as.name(count))
+  )
+
+  lt <- links %>%
+    group_by_(group_field) %>%
+    summarise_(.dots = setNames(dots, c("Model Flow", "Observed Flow")))
+
+  #totals row
+  dots[[3]] <- lazyeval::interp(~as.character(x), x = "Total")
+
+  tot <- links %>%
+    ungroup() %>%
+    summarise_(.dots = setNames(
+      dots,  c("Model Flow", "Observed Flow", as.character(group_field))
+    ))
+
+  suppressWarnings(
+    # this will complain because we are joining a factor to a
+    # character. don't need to worry
+    bind_rows(lt, tot) %>%
+      mutate(
+        `Percent Deviation` = pct_error(`Model Flow`, `Observed Flow`)
+      )
+  )
+
+}
