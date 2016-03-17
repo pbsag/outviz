00de909a9644e5e255c60e318dd24240efbd2869
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-03-17 14:26:19 -0400

    Add rmse functions

--- R/helpers.R
+++ R/helpers.R
@@ -10,3 +10,25 @@
 pct_error <- function(x, y) {
   (x - y) / y * 100
 }
+
+#' Calculate RMSE
+#'
+#' @inheritParams pct_error
+#'
+#' @return The root mean squared error between \code{x} and \code{y}
+#'
+rmse <- function(x, y){
+  n <- length(x)
+  sq_error <- ( x - y )^2
+  sqrt( sum(sq_error) / (n - 1) )
+}
+
+#' Calculate percent RMSE
+#'
+#' @inheritParams pct_error
+#'
+#' @return The percent root mean squared error between \code{x} and \code{y}
+#'
+pct_rmse <- function(x, y){
+  rmse(x, y) / mean(y) * 100
+}
