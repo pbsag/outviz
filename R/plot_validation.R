0dcd95c44039906cc3293f4f6218056527129227
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-03-18 14:32:06 -0400

    Display regression equation on plot

--- R/plot_validation.R
+++ R/plot_validation.R
@@ -9,6 +9,10 @@
 #'   in the link table.
 #' @param color_field (Optional) character string identifying variable to color
 #'   observations by, for example facility type.
+#' @param show_lm If \code{TRUE}, plot a linear model regression fit alongside
+#'   the fitted model's equation. Default \code{FALSE} will print spline
+#'   regression fit without any equation.
+#'
 #'
 #' @return A ggplot2 plot object
 #'
@@ -17,11 +21,12 @@
 #' @import lazyeval
 #'
 #' @examples
-#' plot_validation(links, "volume", "count")
+#' plot_validation(links, "volume", "count", show_lm = TRUE)
 #' plot_validation(links, "volume", "count", color_field = "facility_group")
 #'
 #' @export
-plot_validation <- function(links, volume, count, color_field = NULL) {
+plot_validation <- function(links, volume, count, color_field = NULL,
+                            show_lm = FALSE) {
 
   # if split by color, then add factor variable of the color field
   if(!is.null(color_field)){
@@ -37,13 +42,22 @@ plot_validation <- function(links, volume, count, color_field = NULL) {
   }
 
   # Add geometries for points and statistics, and return
-  p +
-    geom_point(alpha = 0.7) +
+  p <- p +
     geom_abline(slope = 1, intercept = 0) +
-    stat_smooth() +
-
+    geom_point(alpha = 0.7) +
     # Add labels
     ylab("Modeled link volume") +
     xlab("Observed link volume")
+
+  if(show_lm){
+    p +
+      stat_smooth_func(
+        geom = "text", method = "lm",
+        hjust = 0,
+        parse = TRUE) +
+      geom_smooth(method = "lm", se = ifelse(is.null(color_field), TRUE, FALSE))
+  } else {
+    p + geom_smooth()
+  }
 
 }
