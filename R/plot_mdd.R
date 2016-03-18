0770aa4b9adad8e98f6125700bfc83572119be63
Greg Macfarlane <gregmacfarlane@gmail.com>
2016-03-18 09:13:16 -0400

    Handle high error counts in `plot_mdd`

--- R/plot_mdd.R
+++ R/plot_mdd.R
@@ -24,6 +24,13 @@ plot_mdd <- function(links, volume, count, color_field = NULL) {
         y = as.name(count)
     ))
 
+  # super high-error links, which can arise because of mistakes in the counts,
+  # will disrupt the smoothing. Remove these and throw a warning
+  if(any(links$error > 1e3)){
+    warning("Some links have extremely high error. Confirm the count data.")
+    links <- links %>% filter(error < 1e3)
+  }
+
   # if split by color, then add factor variable of the color field
   if(!is.null(color_field)){
     links <- links %>%
@@ -41,7 +48,7 @@ plot_mdd <- function(links, volume, count, color_field = NULL) {
   p +
     geom_point(alpha = 0.7) +
     geom_line(data = mdd, aes_string(x = "volume", y = "mdd", color = NULL)) +
-    coord_cartesian(ylim = c(0, 100), xlim = c(0, max(links[, volume]))) +
+    coord_cartesian(ylim = c(0, 125), xlim = c(0, max(links[, volume]))) +
     stat_smooth() +
 
     # Add labels
