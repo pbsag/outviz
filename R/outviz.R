ae66de8f882f3472f120d1fbb58d2eacb2891488
Greg Macfarlane <macfarlaneg@pbworld.com>
2017-01-04 11:20:40 -0500

    Increment version and document package

---
+++ R/outviz.R
@@ -0,0 +1,9 @@
+#' outviz: A package for uniform and easy model output reporting
+#'
+#' This package reduces repetitive table generation and validation plot outputs
+#' to easy to use functions that can be included in interactive documents.
+#'
+#' @docType package
+#' @name outviz
+#'
+NULL
