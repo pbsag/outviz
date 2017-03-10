b28d584b1a0a71f96b970007ed44a07a12238614
Greg Macfarlane <macfarlaneg@pbworld.com>
2017-03-10 13:50:40 -0500

    Begin tlfd functions
    This serves as a beginning for #35

---
+++ R/tlfd.R
@@ -0,0 +1,34 @@
+#' Plot a trip length frequency distribution comparison
+#'
+#' @param model A dataframe with a distance field and a frequency field representing
+#'   the model or current scenario.
+#' @param target A dataframe with a distance field and a frequency field representing
+#'   the target or reference scenario.
+#'
+#'
+#' @return A ggplot2 object
+#'
+#'
+#'
+#' @export
+#'
+plot_tlfd <- function(model, target){
+
+
+}
+
+
+
+#' Plot a trip length frequency distribution comparison as an interactive figure
+#'
+#' @inheritParams plot_tlfd
+#'
+#'
+#' @return A plotly object
+#'
+#' @export
+#'
+plotly_tlfd <- function(model, target){
+
+
+}
